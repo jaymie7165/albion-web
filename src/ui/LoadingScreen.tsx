635199cccf445e4773152b335948c669ad7bdc8e
jaymie7165 <jjakub.managment@gmail.com>
2026-07-01 12:54:34 +0200

    Delete src directory

--- src/ui/LoadingScreen.tsx
+++
@@ -1,8 +0,0 @@
-export function LoadingScreen() {
-  return (
-    <div className="a3d-loading">
-      <div className="a3d-loading-crest">A</div>
-      <div className="a3d-loading-text">Vstupuji do Albion World…</div>
-    </div>
-  );
-}
